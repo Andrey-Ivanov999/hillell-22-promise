eeeb76f623d78543142d0acbda95e60f4a35eced
redViper <ai830539@gmail.com>
2020-06-23 02:18:10 +0300

    homework changes

--- hillel homework promise work 22 .js
+++ hillel homework promise work 22 .js
@@ -1,49 +1,73 @@
 const promise = new Promise((resolve, reject) => {
-    // resolve(); // call first callback in then()
-    console.log(0);
-    reject(); // call second callback in then()
+    //resolve(); // call first callback in then()
+   console.log(0);
+  reject(); // call second callback in then()
 });
 
 promise
-    .then(() => {
-        // console.log('1 then --- > PROMSE __ RESOLVED');
+   .then(() => {
+       // console.log('1 then --- > PROMSE __ RESOLVED');
 
-        console.log(1);
-        console.log(3);
-        return Promise.resolve(6);
-    }, () => {
-        console.log(2)
-        return Promise.reject(3);
-    })
+       console.log(1);
+       return Promise.resolve(6);
+   }, () => {
+       console.log(2);
+       return Promise.reject(3);
+   })
 
-    .then((users) => {
-            console.log(' then --- > PROMSE __ RESOLVED', users);
+   .then((users) => {
+       console.log(3);
+           console.log(' then --- > PROMSE __ RESOLVED', users);
+           return Promise.resolve(8);
+       },
+       (value) => {
+           console.log(' then --- > PROMSE __ REJECTED', value);
+           return Promise.reject(7);
 
-            return Promise.resolve(8);
-        },
-        (value) => {
-            console.log(' then --- > PROMSE __ REJECTED', value);
-            return Promise.reject(7);
+       })
+   .then((users) => {
+           console.log(' then --- > PROMSE __ RESOLVED', users);
 
-        })
-    .then((users) => {
-            console.log(' then --- > PROMSE __ RESOLVED', users);
-            console.log(9)
-            return Promise.resolve(12);
-        },
-        (value) => {
+           return Promise.resolve(12);
+       },
+       (value) => {
 
-            console.log(6);
-            console.log(' then --- > PROMSE __ REJECTED', value);
-            return Promise.reject(9);
+           console.log(6);
+           console.log(' then --- > PROMSE __ REJECTED', value);
+           return Promise.reject(9);
 
-        })
-    .then((users) => {
-            console.log(' then --- > PROMSE __ RESOLVED', users);
+       })
+       .then((users) => {
+           console.log(9)
+           // console.log(' then --- > PROMSE __ RESOLVED', users);
+           // console.log(9)
+           // return Promise.resolve(12);
+       },
+       (value) => {
 
-        },
-        (value) => {
+           //console.log(6);
+           console.log(' then --- > PROMSE __ REJECTED', value);
+           return Promise.reject(9);
 
-            console.log(' then --- > PROMSE __ REJECTED', value);
-            console.log(12);
-        });
+       })
+   .then((users) => {
+           // console.log(' then --- > PROMSE __ RESOLVED', users);
+           // console.log(9)
+           return Promise.resolve(12);
+       },
+       (value) => {
+
+          // console.log(6);
+         //  console.log(' then --- > PROMSE __ REJECTED', value);
+           return Promise.reject(9);
+
+       })
+   .then((users) => {
+           console.log(' then --- > PROMSE __ RESOLVED', users);
+
+       },
+       (value) => {
+
+          // console.log(' then --- > PROMSE __ REJECTED', value);
+           console.log(12);
+       });
